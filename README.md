d74adb42d3a7cb53346dbe1c1fe3c2e880bb78fc
Cheng Jiang <jiangcheng1214@gmail.com>
2021-03-15 01:35:35 -0700

    wip

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # lindy
 An awesome crawler on Hermes
+
+**Setup
+
+python3 -m venv env
+source env/bin/activate
---
+++ constants.py
@@ -0,0 +1,3 @@
+HERMES_HOME_URL="https://www.hermes.com/us/en/"
+HERMES_CATEGORIES_API="https://bck.hermes.com/categories?locale={}"
+# HERMES_PRODUCT_https://bck.hermes.com/products?locale=us_en&category=MENREADYTOWEAR&offset=0
