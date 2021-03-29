9d29b2ae7814a1ed85572a4c1e7b03989f517022
Cheng Jiang <jiangcheng1214@gmail.com>
2021-03-28 23:41:55 -0700

    dev

--- constants.py
+++ constants.py
@@ -1,3 +1,5 @@
-HERMES_HOME_URL="https://www.hermes.com/us/en/"
-HERMES_CATEGORIES_API="https://bck.hermes.com/categories?locale={}"
-# HERMES_PRODUCT_https://bck.hermes.com/products?locale=us_en&category=MENREADYTOWEAR&offset=0
+PRODUCT_PAGE_SIZE = 108
+
+HERMES_HOME_URL = "https://www.hermes.com/us/en/"
+HERMES_CATEGORIES_API = "https://bck.hermes.com/categories?locale={}"
+HERMES_PRODUCT_API = "https://bck.hermes.com/products?locale={}&category={}&pagesize={}&offset={}"
