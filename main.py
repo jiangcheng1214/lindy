c7f4c9f6f7e7c33561b0106e780bd21cf3f4686d
Cheng Jiang <jiangcheng1214@gmail.com>
2021-06-04 15:30:12 -0700

    update retry

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from EmailSender import EmailSender
 from ScrapeTask import ScrapeTask
+from Utils import wait_random
 
-retry = False
-while not retry:
+retry = 0
+retry_limit = 2
+while retry < retry_limit:
     try:
         task = ScrapeTask(iterations=-1, interval_seconds=60 * 3, debug=False, on_proxy=False)
         task.start()
     except Exception as e:
         emailSender = EmailSender()
         emailSender.notice_admins_on_exception(e, retry)
-        retry = True
+        retry += 1
+        wait_random(2, 3)
