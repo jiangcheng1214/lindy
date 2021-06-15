70bc13b5bbd3d7c5efa5a3d537b7f883d1a8efa1
Cheng Jiang <jiangcheng1214@gmail.com>
2021-06-15 00:43:48 -0700

    timeout

--- Utils.py
+++ Utils.py
@@ -1,19 +1,48 @@
 import inspect
 import logging
-import os
 import shutil
 from datetime import datetime
 from pytz import timezone, utc
 import random
 import time
+from functools import wraps
+import errno
+import os
+import signal
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s | %(name)s | %(levelname)s | %(message)s')
+
 
 class SlowIPException(Exception):
     pass
 
+
 class BlockedIPException(Exception):
     pass
+
+
+class TimeoutError(Exception):
+    pass
+
+
+def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
+    def decorator(func):
+        def _handle_timeout(signum, frame):
+            raise TimeoutError(error_message)
+
+        def wrapper(*args, **kwargs):
+            signal.signal(signal.SIGALRM, _handle_timeout)
+            signal.alarm(seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+            return result
+
+        return wraps(func)(wrapper)
+
+    return decorator
+
 
 def get_current_pst_format_timestamp():
     return datetime.now(tz=utc).astimezone(timezone('US/Pacific')).strftime("%Y%m%d_%H_%M_%S")
--- main.py
+++ main.py
@@ -2,7 +2,56 @@ import sys
 
 from EmailSender import EmailSender
 from ScrapeTask import ScrapeTask
-from Utils import SlowIPException, log_exception, supported_locales, BlockedIPException
+from Utils import SlowIPException, log_exception, supported_locales, BlockedIPException, timeout, TimeoutError
+
+
+@timeout(3600 * 4)
+def scrape_backup():
+    blocked = False
+    while not blocked:
+        try:
+            task = ScrapeTask(local_code, interval_seconds=60 * 3, debug=False, on_proxy=False)
+            task.start()
+        except SlowIPException as e:
+            log_exception(e)
+        except BlockedIPException as e:
+            log_exception(e)
+            if job_type == "backup_scraping":
+                email_sender = EmailSender()
+                email_sender.notice_admins_on_exception(e, local_code, job_type)
+            blocked = True
+        except TimeoutError:
+            print('timeout!')
+            sys.exit(0)
+        except Exception as e:
+            log_exception(e)
+            email_sender = EmailSender()
+            email_sender.notice_admins_on_exception(e, local_code, job_type)
+        finally:
+            task.terminate_scraper()
+
+
+def scrape():
+    blocked = False
+    while not blocked:
+        try:
+            task = ScrapeTask(local_code, interval_seconds=60 * 3, debug=False, on_proxy=False)
+            task.start()
+        except SlowIPException as e:
+            log_exception(e)
+        except BlockedIPException as e:
+            log_exception(e)
+            if job_type == "backup_scraping":
+                email_sender = EmailSender()
+                email_sender.notice_admins_on_exception(e, local_code, job_type)
+            blocked = True
+        except Exception as e:
+            log_exception(e)
+            email_sender = EmailSender()
+            email_sender.notice_admins_on_exception(e, local_code, job_type)
+        finally:
+            task.terminate_scraper()
+
 
 n = len(sys.argv)
 print("Total arguments passed:", n)
@@ -14,23 +63,11 @@ if local_code not in supported_locales():
     sys.exit(-1)
 
 job_type = sys.argv[2]
+
 if job_type == "scraping":
-    blocked = False
-    while not blocked:
-        try:
-            task = ScrapeTask(local_code, interval_seconds=60*3, debug=False, on_proxy=False)
-            task.start()
-        except SlowIPException as e:
-            log_exception(e)
-        except BlockedIPException as e:
-            log_exception(e)
-            blocked = True
-        except Exception as e:
-            log_exception(e)
-            email_sender = EmailSender()
-            email_sender.notice_admins_on_exception(e, local_code, job_type)
-        finally:
-            task.terminate_scraper()
+    scrape()
+elif job_type == "backup_scraping":
+    scrape_backup()
 elif job_type == "updating":
     # TODO:
     pass
